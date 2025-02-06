1a8e17512c160a9293b23bb4542c50a7a1113615
KhangSE184903 <khangtdse184903@fpt.edu.vn>
2025-02-06 14:41:08 +0700

    Buoi 6
    Buoi 6

---
+++ WebApplication_06/src/java/models/User.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author PC
+ */
+public class User {
+    private String userID;
+    private String fullName;
+    private String roleID;
+    private String passWord;
+    private String address;
+
+    public User() {
+    }
+
+    public User(String userID, String fullName, String roleID, String passWord, String address) {
+        this.userID = userID;
+        this.fullName = fullName;
+        this.roleID = roleID;
+        this.passWord = passWord;
+        this.address = address;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getRoleID() {
+        return roleID;
+    }
+
+    public void setRoleID(String roleID) {
+        this.roleID = roleID;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+
+
+}
+
+
+
+
+
